2dc442aeaee6fef90c5a4dfa232e5d478e94947f
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2025-02-14 15:28:28 +0530

    initial commit: ininalize the project with expo native framework

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className="text-2xl text-center text-red-300">RootLayout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
