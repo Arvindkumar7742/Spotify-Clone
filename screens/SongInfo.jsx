2b152f6177486fd46418f4db493c56fad1239def
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2025-03-01 23:19:01 +0530

    Able to navigate to the song info page using uri scheme

--- screens/SongInfo.jsx
+++ screens/SongInfo.jsx
@@ -22,6 +22,7 @@ import {
   removeTrackFromCurrentUser,
   saveTrackForCurrentUser,
 } from "../services/operations/user";
+import { getTrackData } from "../services/operations/track";
 
 const SongInfo = () => {
   const navigation = useNavigation();
@@ -64,106 +65,121 @@ const SongInfo = () => {
     }
   }
 
-  useEffect(() => {}, [route.params]);
+  useEffect(() => {
+    async function fetchTrackData(id) {
+      try {
+        const response = await getTrackData(id);
+        if (response) {
+          console.log("printing the response::==>>", response);
+          setTrackData(response);
+          setIsLiked(
+            likedTracks.map((item) => item.track.id).includes(response.id)
+          );
+        }
+      } catch (err) {
+        Alert.alert("Error", err.message);
+      }
+    }
+    if (id) {
+      fetchTrackData(id);
+    }
+  }, [route.params]);
 
   return (
-    <>
-      <Text>Song Info:{id}</Text>
-    </>
-    // <LinearGradient
-    //   colors={["#F3FF33", "black"]}
-    //   start={{ x: 0.2, y: -0.7 }} // Start at the top
-    //   end={{ x: 0.2, y: 0.85 }} // End at the bottom
-    //   className="flex-1 justify-end items-center"
-    // >
-    //   <View className="flex w-[85%] h-full">
-    //     <TouchableOpacity onPress={() => navigation.goBack()} className="mt-4">
-    //       <Ionicons name="arrow-back-outline" size={28} color="white" />
-    //     </TouchableOpacity>
-    //     <View className="items-center mt-18">
-    //       <Image
-    //         source={{
-    //           uri: item?.album?.images?.[0]?.url
-    //             ? item?.album?.images?.[0]?.url
-    //             : "https://static.vecteezy.com/system/resources/previews/002/249/673/non_2x/music-note-icon-song-melody-tune-flat-symbol-free-vector.jpg",
-    //         }}
-    //         className="w-[100%] h-72 rounded-md mt-10"
-    //         resizeMode="cover"
-    //       />
-    //     </View>
+    <LinearGradient
+      colors={["#F3FF33", "black"]}
+      start={{ x: 0.2, y: -0.7 }} // Start at the top
+      end={{ x: 0.2, y: 0.85 }} // End at the bottom
+      className="flex-1 justify-end items-center"
+    >
+      <View className="flex w-[85%] h-full">
+        <TouchableOpacity onPress={() => navigation.goBack()} className="mt-4">
+          <Ionicons name="arrow-back-outline" size={28} color="white" />
+        </TouchableOpacity>
+        <View className="items-center mt-18">
+          <Image
+            source={{
+              uri: trackData?.album?.images?.[0]?.url
+                ? trackData?.album?.images?.[0]?.url
+                : "https://static.vecteezy.com/system/resources/previews/002/249/673/non_2x/music-note-icon-song-melody-tune-flat-symbol-free-vector.jpg",
+            }}
+            className="w-[100%] h-72 rounded-md mt-10"
+            resizeMode="cover"
+          />
+        </View>
 
-    //     <View className="flex flex-row justify-between mt-10">
-    //       <View className="mt-6">
-    //         <Text className="text-white text-xl font-bold">
-    //           {item?.name.length > 20
-    //             ? item?.name.slice(0, 20) + "..."
-    //             : item?.name}
-    //         </Text>
-    //         <View className="flex flex-row items-center gap-1">
-    //           <Ionicons
-    //             name="sparkles-sharp"
-    //             size={10}
-    //             color="#1dd661"
-    //             className="mt-1"
-    //           />
-    //           <Text
-    //             numberOfLines={1}
-    //             ellipsizeMode="tail"
-    //             className="text-gray-400 text-sm mt-1 w-[180px]"
-    //           >
-    //             {item?.artists?.map((artist) => artist.name).join(", ")}
-    //           </Text>
-    //         </View>
-    //         <View className="flex flex-row items-center gap-1 mt-2">
-    //           <MaterialCommunityIcons name="album" size={20} color="#1dd661" />
-    //           <Text className="text-gray-400 text-sm ">
-    //             {item?.album?.name.length > 25
-    //               ? item?.album?.name.slice(0, 25) + "..."
-    //               : item?.album?.name}
-    //           </Text>
-    //         </View>
-    //       </View>
-    //       <View className="flex flex-row gap-6 items-center">
-    //         <Ionicons name="add-circle-outline" size={35} color="white" />
-    //         <Pressable onPress={handleSongLike}>
-    //           <AntDesign
-    //             name="heart"
-    //             size={24}
-    //             color={isLiked ? "#1DB954" : "white"}
-    //           />
-    //         </Pressable>
-    //       </View>
-    //     </View>
+        <View className="flex flex-row justify-between mt-10">
+          <View className="mt-6">
+            <Text className="text-white text-xl font-bold">
+              {trackData?.name.length > 20
+                ? trackData?.name.slice(0, 20) + "..."
+                : trackData?.name}
+            </Text>
+            <View className="flex flex-row items-center gap-1">
+              <Ionicons
+                name="sparkles-sharp"
+                size={10}
+                color="#1dd661"
+                className="mt-1"
+              />
+              <Text
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                className="text-gray-400 text-sm mt-1 w-[180px]"
+              >
+                {trackData?.artists?.map((artist) => artist.name).join(", ")}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center gap-1 mt-2">
+              <MaterialCommunityIcons name="album" size={20} color="#1dd661" />
+              <Text className="text-gray-400 text-sm ">
+                {trackData?.album?.name.length > 25
+                  ? trackData?.album?.name.slice(0, 25) + "..."
+                  : trackData?.album?.name}
+              </Text>
+            </View>
+          </View>
+          <View className="flex flex-row gap-6 items-center">
+            <Ionicons name="add-circle-outline" size={35} color="white" />
+            <Pressable onPress={handleSongLike}>
+              <AntDesign
+                name="heart"
+                size={24}
+                color={isLiked ? "#1DB954" : "white"}
+              />
+            </Pressable>
+          </View>
+        </View>
 
-    //     <View className="mt-8">
-    //       <ProgressBar
-    //         progress={Math.random()}
-    //         color="#1dd661"
-    //         className="h-1 rounded-full"
-    //       />
-    //       <View className="flex-row justify-between mt-1">
-    //         <Text className="text-gray-400 text-xs">3:06</Text>
-    //         <Text className="text-gray-400 text-xs">3:44</Text>
-    //       </View>
-    //     </View>
+        <View className="mt-8">
+          <ProgressBar
+            progress={Math.random()}
+            color="#1dd661"
+            className="h-1 rounded-full"
+          />
+          <View className="flex-row justify-between mt-1">
+            <Text className="text-gray-400 text-xs">3:06</Text>
+            <Text className="text-gray-400 text-xs">3:44</Text>
+          </View>
+        </View>
 
-    //     <View className="flex flex-row w-[100%] items-center justify-center gap-8">
-    //       <MaterialCommunityIcons
-    //         name="skip-previous"
-    //         size={40}
-    //         color="white"
-    //       />
-    //       <TouchableOpacity onPress={() => setToggle(!toggle)}>
-    //         {toggle ? (
-    //           <Ionicons name="play-circle-sharp" size={75} color="white" />
-    //         ) : (
-    //           <Ionicons name="pause-circle" size={75} color="white" />
-    //         )}
-    //       </TouchableOpacity>
-    //       <MaterialIcons name="skip-next" size={40} color="white" />
-    //     </View>
-    //   </View>
-    // </LinearGradient>
+        <View className="flex flex-row w-[100%] items-center justify-center gap-8">
+          <MaterialCommunityIcons
+            name="skip-previous"
+            size={40}
+            color="white"
+          />
+          <TouchableOpacity onPress={() => setToggle(!toggle)}>
+            {toggle ? (
+              <Ionicons name="play-circle-sharp" size={75} color="white" />
+            ) : (
+              <Ionicons name="pause-circle" size={75} color="white" />
+            )}
+          </TouchableOpacity>
+          <MaterialIcons name="skip-next" size={40} color="white" />
+        </View>
+      </View>
+    </LinearGradient>
   );
 };
 
